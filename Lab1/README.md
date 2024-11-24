b3c2c3748d7885b3bd3fa3e547cfb0ee50a862e9
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-11-24 14:38:09 +0000

    add readme and zip

---
+++ Lab1/README.md
@@ -0,0 +1,15 @@
+# Lab 1
+
+## Build
+
+```bash
+cd build
+cmake CmakeLists.txt
+make
+```
+
+## Run
+
+```bash
+./lab1
+```
---
+++ Lab1/zip.sh
@@ -0,0 +1,7 @@
+mkdir group_25_lab_1
+cp -r src/* group_25_lab_1
+cp -r libraries/* group_25_lab_1
+cp CMakeLists.txt group_25_lab_1
+cp README.md group_25_lab_1
+zip -r group_25_lab_1.zip group_25_lab_1
+rm -rf group_25_lab_1
