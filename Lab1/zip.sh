84c3a910a42c2ddc3fc23b58c7c9e8e40f143bbc
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-11-24 14:40:15 +0000

    fix in zip script

--- Lab1/zip.sh
+++ Lab1/zip.sh
@@ -1,6 +1,6 @@
 mkdir group_25_lab_1
-cp -r src/* group_25_lab_1
-cp -r libraries/* group_25_lab_1
+cp -r src group_25_lab_1
+cp -r libraries group_25_lab_1
 cp CMakeLists.txt group_25_lab_1
 cp README.md group_25_lab_1
 zip -r group_25_lab_1.zip group_25_lab_1
